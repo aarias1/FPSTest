3affe8dff2a570f2280547fb445e3cd52c3db20e
Louis Arias <appenon90@gmail.com>
2017-10-29 22:15:09 -0400

    Patrolling fully converted to C++

--- Source/FPSTest/ChooseNextWaypoint.cpp
+++ Source/FPSTest/ChooseNextWaypoint.cpp
@@ -2,20 +2,33 @@
 
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" //TODO remove coupling
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 
+
+	//TODO protect against no Patrol Route component
+
+
 	//TODO protect against empty patrol routes
 
 
 	//Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) {
+		return EBTNodeResult::Failed;
+	}
+
+	// warn about empty patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0) { 
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points")) 
+			return EBTNodeResult::Failed; 
+	}
 
 	//set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
