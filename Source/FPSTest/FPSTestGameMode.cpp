3a3c887a11fae0743dcc9ebf452560a882a81441
Louis Arias <appenon90@gmail.com>
2017-11-04 13:44:15 -0400

    Cleaning up project files and directories for more simplistic structure

--- Source/FPSTest/FPSTestGameMode.cpp
+++ Source/FPSTest/FPSTestGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "FPSTestGameMode.h"
 #include "FPSTestHUD.h"
-#include "FPSTestCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFPSTestGameMode::AFPSTestGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
